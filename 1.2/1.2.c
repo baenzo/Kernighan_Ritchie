f723f2e2f9b45bee580d1230bc06c08496cc699c
Stanislav <stanislav.kalinkin@gmail.com>
2021-03-31 08:55:15 +0300

    1.2 has been fixed

---
+++ 1.2/1.2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("The bad escape \f sequence.");
+	return 0;
+}
--- 1.2/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("Escape sequences\n\tnew line and tabulation.");
-	return 0;
-}
