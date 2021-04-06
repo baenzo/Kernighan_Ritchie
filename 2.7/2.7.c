62f6f96c9d36eb479042ed8ce52ab79427a560e8
Stanislav <stanislav.kalinkin@gmail.com>
2021-04-06 11:45:09 +0300

    2.7 has been fixed

--- 2.7/2.7.c
+++ 2.7/2.7.c
@@ -4,17 +4,41 @@ int invert(int x, unsigned int p, unsigned int n);
 
 int main()
 {
-	int x = 1903483230;
-	int result = invert(x, 31, 32);
-	printf("%d", result);
+	int x = -1272023937;
+	unsigned int int_width = sizeof(int) * 8;
+
+	printf("x = %d\n", x);
+	printf("int width = %u bits\n\n", int_width);
+
+	char format_str[] = "p = %2u, n = %2u, result = %d\n";
+
+	printf(format_str, int_width - 1, 0, invert(x, int_width - 1, 0));
+	printf(format_str, int_width - 1, 5, invert(x, int_width - 1, 5));
+	printf(format_str, int_width - 1, int_width, invert(x, int_width - 1, int_width));
+
+	printf(format_str, 12, 0, invert(x, 12, 0));
+	printf(format_str, 12, 5, invert(x, 12, 5));
+	printf(format_str, 12, 13, invert(x, 12, 13));
+
+	printf(format_str, 0, 0, invert(x, 0, 0));
+
 	return 0;
 }
 
 int invert(int x, unsigned int p, unsigned int n)
 {
 	int a = ~x;
-	int mask = ((~0 << p) << 1) | (~(~0 << (p + 1 - n)));
-	//                 ^^^^^^^ do not replace with (p + 1) !!!
+	int mask = ~0 << p << 1;
+
+	if (n == 0)
+	{
+		mask |= ~(~0 << p << 1);
+	}
+	else
+	{
+		mask |= ~(~0 << (p + 1 - n));
+	}
+
 	x &= mask;
 	mask = ~mask;
 	a &= mask;
