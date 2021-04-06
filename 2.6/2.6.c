4a5feff99e839d0b82e6ba4fcf82cd718bcc4a48
Stanislav <stanislav.kalinkin@gmail.com>
2021-04-06 10:44:15 +0300

    2.6 has been fixed

--- 2.6/2.6.c
+++ 2.6/2.6.c
@@ -6,21 +6,53 @@ int main()
 {
 	int x = -1272023937;
 	int y = 1999443473;
-	int result = setbits(x, 9, 3, y);
-	printf("%d", result);
+	unsigned int int_width = sizeof(int) * 8;
+
+	printf("x = %d\n", x);
+	printf("y = %d\n", y);
+	printf("int width = %u bits\n\n", int_width);
+
+	char format_str[] = "p = %2u, n = %2u, result = %d\n";
+
+	printf(format_str, int_width - 1, 0, setbits(x, int_width - 1, 0, y));
+	printf(format_str, int_width - 1, 5, setbits(x, int_width - 1, 5, y));
+	printf(format_str, int_width - 1, int_width, setbits(x, int_width - 1, int_width, y));
+	
+	printf(format_str, 12, 0, setbits(x, 12, 0, y));
+	printf(format_str, 12, 5, setbits(x, 12, 5, y));
+	printf(format_str, 12, 13, setbits(x, 12, 13, y));
+
+	printf(format_str, 0, 0, setbits(x, 0, 0, y));
+
 	return 0;
 }
 
 int setbits(int x, unsigned int p, unsigned int n, int y)
 {
-	int x_mask = 0;
-	x_mask = (~x_mask << (p + 1)) | (~(~x_mask << (p + 1 - n)));
-	x &= x_mask;
-
+	int x_mask = ~0 << p << 1;
 	int y_mask = 0;
-	y_mask = ~(~y_mask << n);
-	y &= y_mask;
-	y <<= p + 1 - n;
 
+	if (n == 0)
+	{
+		x_mask |= ~(~0 << p << 1);
+	}
+	else
+	{
+		x_mask |= ~(~0 << (p + 1 - n));
+		y_mask = ~(~0 << (n - 1) << 1);
+	}
+
+	x &= x_mask;
+	y &= y_mask;
+
+	if (n == 0)
+	{
+		y = y << p << 1;
+	}
+	else
+	{
+		y <<= p + 1 - n;
+	}
+	
 	return x | y;
 }
